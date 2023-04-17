4a3edb19842655bc6578b825e8582b89ec128116
giraftw2002 <giraftw2002@gmail.com>
2023-04-17 18:29:26 +0800

    singles underscore for vailables & methods

--- serial_class.py
+++ serial_class.py
@@ -5,17 +5,17 @@ from threading import Thread, Event
 
 class SerialPort():
     def __init__(self):                
-        port = self.__get_serial_port()
+        port = self._get_serial_port()
         if port:
             self.ser = serial.Serial(port, baudrate=115200, timeout=1)
             print(f"Connected to serial port {self.ser.name}")
-            self.__event_ok2send = Event()
-            self.__event_ok2send.set()
+            self._event_ok2send = Event()
+            self._event_ok2send.set()
 
             self.event_run = Event()
             self.event_run.set()
 
-            self.t = Thread(target=self.__ReceiveThread, args=[self.event_run])
+            self.t = Thread(target=self._ReceiveThread, args=[self.event_run])
             self.t.start()
             # do something with the serial connection
         else:
@@ -27,7 +27,7 @@ class SerialPort():
     def clear_event_run(self):
         self.event_run.clear()
 
-    def __get_serial_port(self):
+    def _get_serial_port(self):
         """Automatically detects and returns the serial port."""
         ports = list(serial.tools.list_ports.comports())
         for port in ports:
@@ -47,15 +47,15 @@ class SerialPort():
         # msg = f'{header}{j[0]:.2f},{j[1]:.2f},{j[2]:.2f},{j[3]:.2f},{j[4]:.2f},{j[5]:.2f}\n'
         msg = '{}{:.2f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f}\n'.format(header, *j,)
         self.ser.write(msg.encode('utf-8'))
-        self.__event_ok2send.clear()
+        self._event_ok2send.clear()
         print(msg)
         if bWaitAck is True:
             # wait till the event is set in rcvThread.
-            self.__event_ok2send.wait()            
+            self._event_ok2send.wait()            
         # while event_ack.is_set() and bWaitAck is True:
         #    pass
 
-    def __ReceiveThread(self, event_run):
+    def _ReceiveThread(self, event_run):
         """
         input string is retrieved as a byte string, which works
         differently than a standard string. The decode() method is to convert
@@ -70,4 +70,4 @@ class SerialPort():
                 print(string)
                 if string == 'ack':
                     # receive ack frm arduion meaning it is free now
-                    self.__event_ok2send.set()
+                    self._event_ok2send.set()
