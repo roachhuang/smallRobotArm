047554cd06d3457a75fafaba27f26a54b9c19001
Mark Huang <giraftw2002@gmail.com>
2026-05-17 09:11:06 +0800

    fix typo in __init__.py

--- robot_tools/kinematics/__init__.py
+++ robot_tools/kinematics/__init__.py
@@ -6,5 +6,5 @@ This module provides forward and inverse kinematics for the robot arm.
 
 from .robotarm_class import SmallRbtArm
 from .std_dh_table import std_dh_tbl, std_dh_params
-
-__all__ = ['SmallRbtArm', 'std_dh_table', 'std_dh_params']
+import modern_robotics as mr
+__all__ = ['mr', 'SmallRbtArm', 'std_dh_tbl', 'std_dh_params']
